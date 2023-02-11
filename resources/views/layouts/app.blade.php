0c2b09fd14613daf64288d8e99d23e783c3efea6
3bsamad0x90 <mohamedabdelsamad122@gmail.com>
2023-02-11 14:13:24 +0200

    user and post factory and seeder to get fake data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -15,7 +14,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
-
 <body>
     <div id="app">
         @include('layouts.navbar')
@@ -23,8 +21,6 @@
             @yield('content')
         </main>
     </div>
-
-
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
